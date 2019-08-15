8861f56d5a9ef0ae8ff059f5ec68f6ab656a45c6
cigenterprise <cigetp@gmail.com>
2019-08-15 11:14:47 +0900

    [2019_08] 프로젝트 기본 구조

---
+++ Project ColorBreak/Assets/Scripts/Stage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 스테이지 시작 시 실행
+    public void StartStage()
+    {
+
+    }
+
+    public void FinishStage()
+    {
+
+    }
+}
---
+++ Project ColorBreak/Assets/Scripts/StageManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    public Stage stage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ( stage )
+        {
+            stage.StartStage();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
