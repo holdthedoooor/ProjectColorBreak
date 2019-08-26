8219465189fc7ebb2ca93b114c274a625dca2c9c
SBSun <byung0216@gmail.com>
2019-08-26 23:15:35 +0900

    1. 뒤로가기 버튼 추가

--- Project ColorBreak/Assets/Scripts/StageSelectUI.cs
+++ Project ColorBreak/Assets/Scripts/StageSelectUI.cs
@@ -6,5 +6,33 @@ using UnityEngine.UI;
 
 public class StageSelectUI : MonoBehaviour
 {
-    public GameObject go_StageSelectUI;
+    public void Chapter1_Back()
+    {
+        UIManager.instance.chapterSelectUI.go_Chapters[0].SetActive( false );
+        UIManager.instance.chapterSelectUI.go_ChapterSelectUI.SetActive( true );
+    }
+
+    public void Chapter2_Back()
+    {
+        UIManager.instance.chapterSelectUI.go_Chapters[1].SetActive( false );
+        UIManager.instance.chapterSelectUI.go_ChapterSelectUI.SetActive( true );
+    }
+
+    public void Chapter3_Back()
+    {
+        UIManager.instance.chapterSelectUI.go_Chapters[2].SetActive( false );
+        UIManager.instance.chapterSelectUI.go_ChapterSelectUI.SetActive( true );
+    }
+
+    public void Chapter4_Back()
+    {
+        UIManager.instance.chapterSelectUI.go_Chapters[3].SetActive( false );
+        UIManager.instance.chapterSelectUI.go_ChapterSelectUI.SetActive( true );
+    }
+
+    public void Chapter5_Back()
+    {
+        UIManager.instance.chapterSelectUI.go_Chapters[4].SetActive( false );
+        UIManager.instance.chapterSelectUI.go_ChapterSelectUI.SetActive( true );
+    }
 }
