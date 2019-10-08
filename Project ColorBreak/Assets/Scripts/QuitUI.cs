80ce3a3a7d7f7b8696779767b2a1fad841faa23d
SBSun <46879319+SBSun@users.noreply.github.com>
2019-10-08 14:35:26 +0900

    1. 뒤로가기 버튼 구현 - 지금 버전은 WindowsPlatform에서만 가능

---
+++ Project ColorBreak/Assets/Scripts/QuitUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitUI : MonoBehaviour
+{
+    public GameObject go_QuitUI;
+
+    public void YesButton()
+    {
+        Application.Quit();
+    }
+
+    public void NoButton()
+    {
+        go_QuitUI.SetActive( false );
+        Quit.instance.num++;
+    }
+}
