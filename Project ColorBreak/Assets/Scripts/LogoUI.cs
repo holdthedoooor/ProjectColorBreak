5538ab15cc650045a4cce017bc19083a3f6973f9
SBSun <46879319+SBSun@users.noreply.github.com>
2019-10-27 13:25:17 +0900

    1. 로고 추가

---
+++ Project ColorBreak/Assets/Scripts/LogoUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoUI : MonoBehaviour
+{
+    public GameObject go_Logo;
+    
+    void Start()
+    {
+        StartCoroutine( LogoCoroutine() );
+    }
+    
+    public IEnumerator LogoCoroutine()
+    {
+        yield return new WaitForSeconds( 2.5f );
+
+        go_Logo.SetActive( false );
+        UIManager.instance.lobbyUI.go_LobbyUI.SetActive( true );
+    }
+}
